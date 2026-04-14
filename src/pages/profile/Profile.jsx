30a8b5c4ad6083f598185bcf6e5fe2fd7ad02c11
Ralph-Beentjes <ralphsimonbeentjes@gmail.com>
2026-04-14 13:08:29 +0200

    Informatie op de profielpagina toegevoegd

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,32 @@
+import {useContext} from "react";
+import {AuthContext} from "../../context/AuthContext.jsx";
+import './Profile.css';
+
 function Profile(){
+    const { auth } = useContext(AuthContext);
+
     return (
-        <>
-        </>
+        <div className='profile-outer-container'>
+            <section className='personal-details'>
+                <h2>Personal details:</h2>
+                <div className='personal-details-info'>
+                    <span>
+                        <p><strong>Username:</strong></p><p>{auth.user?.preferred_username}</p>
+                    </span>
+                    <span>
+                        <p><strong>First name:</strong></p><p>{auth.user?.given_name}</p>
+                    </span>
+                    <span>
+                        <p><strong>Last name:</strong></p><p>{auth.user?.family_name}</p>
+                    </span>
+                </div>
+            </section>
+            <section className='avatar-container'>
+                <h2>Avatar:</h2>
+                <p>Hier komt een avatar te staan</p>
+                <p>En hier buttons voor uploaden en downloaden</p>
+            </section>
+        </div>
     )
 }
 
